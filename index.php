fc54da0597e2ea73ec58cade1dbf2fb10e903af5
seffany-oliv <steffany.oliv1@gmail.com>
2025-09-26 08:19:54 -0300

    commit steffany - prazo

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
                 <option value="jurosSimples">Juros</option>
                 <option value="capital">Capital</option>
                 <option value="taxa">Taxa</option>
-
+                <option value="prazo">Prazo</option>
             </select>
             
         </p>
@@ -71,14 +71,15 @@
                 echo "<h2 style='text-align: center;'>O valor da taxa é: " . taxa($j, $c, $n) . "%</h2>";
             }
             function prazo($juros, $capital, $taxa) {
-                return ($juros / ($capital * ($taxa / 100)));
+                return ($juros / ($capital * $taxa)/100);
             }
             if ($op == 'prazo'){
                 $prazo = prazo($j, $c, $i);
-                echo "<h2 style='text-align: center;'>O valor da prazo é: " . taxa($j, $c, $i) . "%</h2>";
+                echo "<h2 style='text-align: center;'>O valor da prazo é: " . taxa($j, $c, $i) . " meses</h2>";
 
 
             }
+
         }
     }
     ?>
